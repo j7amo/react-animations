bdf0985b7c52c359663f5f6b8459e18ec7799e34
Roman Tarnakin <roman_tarnakin@epam.com>
2023-01-04 10:01:50 +0300

    finish exploring TransitionGroup component from react-transition-group package

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,6 +1,8 @@
 /* eslint-disable jsx-a11y/click-events-have-key-events,
 jsx-a11y/no-noninteractive-element-interactions */
 import React, { Component } from 'react';
+import TransitionGroup from 'react-transition-group/TransitionGroup';
+import CSSTransition from 'react-transition-group/CSSTransition';
 
 import './List.css';
 
@@ -8,33 +10,41 @@ class List extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      items: [1, 2, 3],
+      items: [
+        {
+          id: Math.random().toString(),
+          value: 1,
+        },
+      ],
     };
   }
 
   addItemHandler = () => {
     this.setState((prevState) => ({
-      items: prevState.items.concat(prevState.items.length + 1),
+      items: prevState.items.concat({
+        id: Math.random().toString(),
+        value: prevState.items.length + 1,
+      }),
     }));
   };
 
-  removeItemHandler = (selIndex) => {
+  removeItemHandler = (id) => {
     this.setState((prevState) => ({
-      items: prevState.items.filter((item, index) => index !== selIndex),
+      items: prevState.items.filter((item) => item.id !== id),
     }));
   };
 
   render() {
     const { items } = this.state;
 
-    const listItems = items.map((item, index) => (
-      <li
-        key={Math.random().toString()}
-        className="ListItem"
-        onClick={() => this.removeItemHandler(index)}
-      >
-        {item}
-      </li>
+    // it is VERY IMPORTANT for list elements to have ID
+    // in order for 'removeItemHandler' to work correctly
+    const listItems = items.map(({ id, value }) => (
+      <CSSTransition key={id} timeout={500} classNames="fade">
+        <li className="ListItem" onClick={() => this.removeItemHandler(id)}>
+          {value}
+        </li>
+      </CSSTransition>
     ));
 
     return (
@@ -43,7 +53,11 @@ class List extends Component {
           Add Item
         </button>
         <p>Click Item to Remove.</p>
-        <ul className="List">{listItems}</ul>
+        {/* by default <TransitionGroup/> renders as a <div>
+         but we can change it via 'component' prop */}
+        <TransitionGroup className="List" component="ul">
+          {listItems}
+        </TransitionGroup>
       </div>
     );
   }
